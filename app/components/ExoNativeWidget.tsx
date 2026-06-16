4e3a4b311bfb6e1400288e059d16031016e81830
Prudence Tchidi <tchidiprudence7@gmail.com>
2026-06-16 14:27:02 +0100

    Fix native placement

--- app/components/ExoNativeWidget.tsx
+++ app/components/ExoNativeWidget.tsx
@@ -6,45 +6,24 @@ export default function ExoNativeWidget() {
 
 return(
 
-<div className="mt-8 mb-8 flex justify-center">
+<div className="mt-8 mb-8 flex justify-center overflow-hidden">
 
-{/* ExoClick Native */}
-<script
-async
+<Script
 src="https://a.magsrv.com/ad-provider.js"
-></script>
+strategy="afterInteractive"
+/>
 
 <ins
 className="eas6a97888e20"
 data-zoneid="5945506"
 ></ins>
 
-<Script id="exo-native">
-{`
-(AdProvider = window.AdProvider || []).push({"serve": {}});
-`}
-</Script>
-
-{/* Hilltop fallback */}
-<Script id="hilltop-native">
+<Script id="exo-native-init">
 
 {`
-(function(pi){
-var d=document,
-s=d.createElement("script"),
-l=d.scripts[d.scripts.length-1];
-
-s.settings=pi||{};
-
-s.src="//grounded-opposite.com/b/XLV.sddsG/l/0ZYUWOcP/reTmx9Su/Z/UklLk/P/TlcsxMNaDMI/xDMFDHk/tJNozQEo0/Mqj/EQxlM/wq";
-
-s.async=true;
-
-s.referrerPolicy="no-referrer-when-downgrade";
-
-l.parentNode.insertBefore(s,l);
-
-})({})
+(AdProvider = window.AdProvider || []).push({
+serve:{}
+});
 `}
 
 </Script>
