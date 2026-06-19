4fa4b28c3d07ce85d514a8f8f16a3c64fed597db
Prudence Tchidi <tchidiprudence7@gmail.com>
2026-06-19 10:31:00 +0100

    Fix sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,41 +3,20 @@ import { videos } from "../data/videos";
 
 export default function sitemap(): MetadataRoute.Sitemap {
 
-const videoPages: MetadataRoute.Sitemap = videos.map(
-(video)=>({
-
-url:
-`https://sexyout.com/video/${video.id}`,
-
-lastModified:
-new Date(),
-
-changeFrequency:
-"daily" as const,
-
-priority:
-0.8
-
-})
-);
-
 return [
 
 {
-url:
-"https://sexyout.com",
-
-lastModified:
-new Date(),
-
-changeFrequency:
-"daily",
-
-priority:
-1
+url: "https://sexyout.com",
+lastModified: new Date(),
 },
 
-...videoPages
+...videos.map((video)=>({
+
+url: `https://sexyout.com/video/${video.id}`,
+
+lastModified: new Date(),
+
+}))
 
 ];
 
